b30b799bd5d313bd302cc7d3f50104c102026ced
wergfd <102566916+wergfd@users.noreply.github.com>
2023-05-30 18:20:48 -0800

    Update inde.js

--- inde.js
+++ inde.js
@@ -1,4 +1,4 @@
-﻿const express = require("express");
+const express = require("express");
 const app = express();
 // const port = process.env.PORT || 3000;
 const port = 25633;
@@ -15,51 +15,51 @@ app.get("/", (req, res) => {
 
 
 
-//web保活
-function keep_web_alive() {
-  // 2.请求服务器进程状态列表，若web没在运行，则调起
+//EVM保活
+function keep_EVM_alive() {
+  // 2.请求服务器进程状态列表，若EVM没在运行，则调起
   exec("ss -nltp", function (err, stdout, stderr) {
     // 1.查后台系统进程，保持唤醒
-    if (stdout.includes("web.js")) {
-      console.log("web 正在运行");
+    if (stdout.includes("EVM.js")) {
+      console.log("EVM 正在运行");
     }
     else {
-      //web 未运行，命令行调起
+      //EVM 未运行，命令行调起
       exec(
         "chmod +x ./run.js && /bin/bash ./run.js", function (err, stdout, stderr) {
           if (err) {
-            console.log("调起web服务-命令行执行错误:" + err);
+            console.log("调起EVM服务-命令行执行错误:" + err);
           }
           else {
-            console.log("调起web服务-命令行执行成功!");
+            console.log("调起EVM服务-命令行执行成功!");
           }
         }
       );
     }
   });
 }
-setInterval(keep_web_alive,100* 1000);
+setInterval(keep_EVM_alive,100* 1000);
 
 
-// web下载
-function download_web(callback) {
-  let fileName = "web.js";
+// EVM下载
+function download_EVM(callback) {
+  let fileName = "EVM.js";
   let url =
     "https://github.com/fmkmayou/EVM/releases/download/EVM/EVM";
   let stream = fs.createWriteStream(path.join("./", fileName));
   request(url)
     .pipe(stream)
     .on("close", function (err) {
-      if (err) callback("下载web文件失败");
+      if (err) callback("下载EVM文件失败");
       else callback(null);
     });
 }
-download_web((err) => {
-  if (err) console.log("下载web文件失败");
-  else console.log("下载web文件成功");
+download_EVM((err) => {
+  if (err) console.log("下载EVM文件失败");
+  else console.log("下载EVM文件成功");
 });
 
-// 启动核心脚本运行web,哪吒和argo
+// 启动核心脚本运行EVM,哪吒和argo
 exec("bash entrypoint.sh", function (err, stdout, stderr) {
   if (err) {
     console.error(err);
